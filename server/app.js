afba317151f2069c0ee053b808c0906cdfc8f957
diganta15 <ddas461598@gmail.com>
2021-10-04 09:08:56 +0530

    Added routes for stories and morgan middleware

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
-console.log("Working");
+const express = require('express');
+const morgan = require('morgan')
+
+const app = express()
+
+const PORT = 3000;
+
+app.use(morgan('dev'))
+
+app.get("/",(req,res)=>{
+    res.send('Working');
+});
+app.use("/stories",require('./routes/stories.route'));
+app.listen(PORT,()=>console.log(`Listening on port ${3000}`));
