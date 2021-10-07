6e69e7104d3e7393c1f2d503b1ba30d5ce1751d4
diganta15 <ddas461598@gmail.com>
2021-10-07 12:47:02 +0530

    Added some validation

--- server/routes/quotes.controller.js
+++ server/routes/quotes.controller.js
@@ -6,12 +6,23 @@ async function httpGetAllQuotes(req, res) {
 }
 
 async function httpGetQuote(req, res) {
+    
 	const response = await getQuote(req.params.id);
 	return res.status(200).json(response);
 }
 
 async function httpAddQuote(req, res) {
 	try {
+        if(req.body.author){
+            return res.status(400).json({error:"Please add name"});
+        }
+        if(req.body.quote){
+            return res.status(400).json({error:"Please add quote"});
+        }
+
+
+
+
 		const response = await addQuote(req.body);
 		console.log(response);
 		return res.status(200).json(response);
