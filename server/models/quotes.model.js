5d756384c676765ddcdc54743dbab1799e33b8a1
diganta15 <ddas461598@gmail.com>
2021-10-07 12:38:24 +0530

    Can add quote and fetch quotes

--- server/models/quotes.model.js
+++ server/models/quotes.model.js
@@ -1,54 +1,77 @@
-const quote = require('../models/quote.mongo');
+const quote = require("../models/quote.mongo");
 
 const quotes = [
-    {
-        id: "1",
-        author: "Albert Camus",
-        quote: "In the depth of winter, I finally learned that within me there lay an invincible summer..",
-        date: new Date(),
-    },
-    {
-        id: "2",
-        author: "Albert Camus",
-        quote: "The struggle itself towards the heights is enough to fill a man's heart.One must imagine Sisyphus happy.",
-        date: new Date(),
-    },
-    {
-        id:"3",
-        author:"Albert Camus",
-        quote:"Autumn is a second spring when every leaf is a flower.",
-        date: new Date(),
+	{
+		id: "1",
+		author: "Albert Camus",
+		quote:
+			"In the depth of winter, I finally learned that within me there lay an invincible summer..",
+		date: new Date(),
+	},
+	{
+		id: "2",
+		author: "Albert Camus",
+		quote:
+			"The struggle itself towards the heights is enough to fill a man's heart.One must imagine Sisyphus happy.",
+		date: new Date(),
+	},
+	{
+		id: "3",
+		author: "Albert Camus",
+		quote: "Autumn is a second spring when every leaf is a flower.",
+		date: new Date(),
+	},
+];
+
+async function getAllQuotes() {
+	try {
+		const res = await quote.find({}).sort({ date: -1 });
+		return res;
+	} catch (err) {
+		const error = { error: "Cannot get data from database" };
+		return error;
+	}
+}
+
+async function getQuote(id) {
+	try{
+        const res = await quote.findOne({_id:id});
+        return res;
     }
-]
-
-function getAllQuotes(){
-    return quotes;
+    catch(err){
+        const error = {error:"Cannot get quote"};
+        return error;
+    }
 }
 
-function getQuote(id){
-    return quotes.filter(quote => quote.id === id);
+async function saveData(data) {
+	try {
+		const newQuote = new quote({
+			...data,
+		});
+		const res = await newQuote.save(data);
+
+		return res;
+	} catch (err) {
+		const error = {
+			error: "Cannot save data to database",
+		};
+		return error;
+	}
 }
 
-async function saveData(data){
-    console.log(data);
+async function addQuote(quote) {
+	const quoteWithDefaultFields = {
+		...quote,
+		date: new Date(),
+	};
 
-    const newQuote = new quote(data);
-
-   const res = await newQuote.save(data);
-   return res;
-}
-
-async function addQuote(quote){
-    const quoteWithDefaultFields = {
-        ...quote,
-        date:new Date(),
-    }  
-    return saveData(quoteWithDefaultFields);
-   
+	const res = await saveData(quoteWithDefaultFields);
+	return res;
 }
 
 module.exports = {
-    getAllQuotes,
-    getQuote,
-    addQuote,
-}
+	getAllQuotes,
+	getQuote,
+	addQuote,
+};
--- server/routes/quotes.controller.js
+++ server/routes/quotes.controller.js
@@ -1,24 +1,27 @@
-const {getAllQuotes, getQuote, addQuote} = require('../models/quotes.model');
+const { getAllQuotes, getQuote, addQuote } = require("../models/quotes.model");
 
-async function httpGetAllQuotes(req,res){
-    const response = await getAllQuotes();
-
-    return res.status(200).json(response);
+async function httpGetAllQuotes(req, res) {
+	const response = await getAllQuotes();
+	return res.status(200).json(response);
 }
 
-async function httpGetQuote(req,res){
-        const response = await getQuote(req.params.id);
-
-        return res.status(200).json(response);
+async function httpGetQuote(req, res) {
+	const response = await getQuote(req.params.id);
+	return res.status(200).json(response);
 }
 
-async function httpAddQuote(req,res){
-    
-    await res.status(200).json(addQuote(req.body));
+async function httpAddQuote(req, res) {
+	try {
+		const response = await addQuote(req.body);
+		console.log(response);
+		return res.status(200).json(response);
+	} catch (err) {
+		await res.status(500).json({ error: "Server Error" });
+	}
 }
 
 module.exports = {
-    httpGetAllQuotes,
-    httpGetQuote,
-    httpAddQuote,
-}
+	httpGetAllQuotes,
+	httpGetQuote,
+	httpAddQuote,
+};
