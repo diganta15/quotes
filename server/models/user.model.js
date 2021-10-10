7e6f5ce02c0413999da1e2f74a80f7202fbb8280
diganta15 <ddas461598@gmail.com>
2021-10-10 20:05:35 +0530

    Added routes for user creation

---
+++ server/models/user.model.js
@@ -0,0 +1,14 @@
+const user = require('./user.mongo');
+const bcrypt = require('bcrypt');
+
+const salt = process.env.salt;
+
+async function hashPassword(password){
+    console.log
+}
+
+async function signup(data){
+    // const userObj = {
+    //     ...
+    // }
+}
