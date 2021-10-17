1582e23a749d3f45ef3828291f766c882ccac0ee
diganta15 <ddas461598@gmail.com>
2021-10-17 22:04:02 +0530

    Added validation for login

--- server/routes/quotes.controller.js
+++ server/routes/quotes.controller.js
@@ -6,23 +6,18 @@ async function httpGetAllQuotes(req, res) {
 }
 
 async function httpGetQuote(req, res) {
-    
 	const response = await getQuote(req.params.id);
 	return res.status(200).json(response);
 }
 
 async function httpAddQuote(req, res) {
 	try {
-        if(!req.body.author){
-            return res.status(400).json({error:"Please add author"});
-        }
-        if(!req.body.quote){
-            return res.status(400).json({error:"Please add quote"});
-        }
-
-
-
-
+		if (!req.body.author) {
+			return res.status(400).json({ error: "Please add author" });
+		}
+		if (!req.body.quote) {
+			return res.status(400).json({ error: "Please add quote" });
+		}
 		const response = await addQuote(req.body);
 		console.log(response);
 		return res.status(200).json(response);
--- server/routes/user.controller.js
+++ server/routes/user.controller.js
@@ -1,45 +1,45 @@
-const {signup, login} = require('../models/user.model');
+const { signup, login } = require("../models/user.model");
 
-async function httpAddUser(req,res){
-    if(!req.body.name){
-        return res.status(400).json({"error":"Please add name"});
-    }
-    if(!req.body.email){
-        return res.status(400).json({"error":"Please add email"});
-    }
-    if(!req.body.password){
-        return res.status(400).json({"error":"Please add password"});
-    }
+async function httpAddUser(req, res) {
+	if (!req.body.name) {
+		return res.status(400).json({ error: "Please add name" });
+	}
+	if (!req.body.email) {
+		return res.status(400).json({ error: "Please add email" });
+	}
+	if (!req.body.password) {
+		return res.status(400).json({ error: "Please add password" });
+	}
 
-    try{
-        const response = await signup(req.body);
-        return res.status(200).json(response);
-    }
-    catch(err){
-       
-        return res.status(500).json({error:"Internal Server Error"});
-    }
-
-
-
+	try {
+		const response = await signup(req.body);
+		return res.status(200).json(response);
+	} catch (err) {
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
 }
 
-async function httpLogInUser(req,res){
-
-    try{
-       return res.status(200).json(await login(req.body));
-    }catch(err){
-        return res.status(500).json({error:"Internal Server Error"});
-    }
-
+async function httpLogInUser(req, res) {
+	try {
+        if(!req.body.email){
+            return res.status(400).json({error:"Please enter your email"});
+            
+        }
+        if(!req.body.password){
+            return res.status(400).json({error:"Please enter your password"});
+        }
+		return res.status(200).json(await login(req.body));
+	} catch (err) {
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
 }
 
-async function httpLogOutUser(req,res){
-    res.send("log out")
+async function httpLogOutUser(req, res) {
+	res.send("log out");
 }
 
 module.exports = {
-    httpAddUser,
-    httpLogInUser,
-    httpLogOutUser
-}
+	httpAddUser,
+	httpLogInUser,
+	httpLogOutUser,
+};
